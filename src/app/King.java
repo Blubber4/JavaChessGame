be5ef61c61d9e13dec5d410fba22f7a96d37563b
Tony <48278185+MugiPham@users.noreply.github.com>
2020-04-19 16:55:48 -0500

    King

---
+++ src/app/King.java
@@ -0,0 +1,14 @@
+package app;
+
+public class King extends ChessPiece
+{
+  public King()
+  {
+    super("King.png", 0, 0);
+  }
+
+  public King(int x, int y)
+  {
+    super("King.png", x, y);
+  }
+}
