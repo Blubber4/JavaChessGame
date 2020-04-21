a8eda1307bbce74452140d227cdb25ca2163cdd7
Blub <mr.millian@gmail.com>
2020-04-21 18:36:30 -0500

    formatting cleanup, deleted some unused code

--- src/app/Bishop.java
+++ src/app/Bishop.java
@@ -3,27 +3,26 @@ package app;
 import java.util.ArrayList;
 
 /**
- * This is the Bishop Class.
- * The Move Function defines the basic rules for movement of Bishop on a chess board
+ * This is the Bishop Class. The Move Function defines the basic rules for
+ * movement of Bishop on a chess board
  * 
  */
 public class Bishop extends ChessPiece {
 
     public Bishop(final String i, final String p, final int c) {
-		setId(i);
-		setPath(p);
-		setColor(c);
+        setId(i);
+        setPath(p);
+        setColor(c);
     }
-    
-    //Move function is defined. It returns a list of all the possible destinations of a Bishop
-	//The basic principle of Bishop Movement on chess board has been implemented
-    public ArrayList<Board> move(final Board state[][], final int x, final int y)
-	{
-		//Bishop can Move diagonally in all 4 direction (NW,NE,SW,SE)
-		//This function defines that logic
-		possiblemoves.clear();
-		//add Bishop moves here
-		
-	}
 
+    // Move function is defined. It returns a list of all the possible destinations
+    // of a Bishop
+    // The basic principle of Bishop Movement on chess board has been implemented
+    public ArrayList<Board> move(final Board state[][], final int x, final int y) {
+        // Bishop can Move diagonally in all 4 direction (NW,NE,SW,SE)
+        // This function defines that logic
+        possiblemoves.clear();
+        // add Bishop moves here
+
+    }
 }
--- src/app/GameMouseListener.java
+++ src/app/GameMouseListener.java
@@ -9,120 +9,40 @@ import javax.swing.event.MouseInputAdapter;
 
 /* mouse listener is attached to main frame, so points will have to be adjusted to consider position relative to the window */
 public class GameMouseListener extends MouseInputAdapter {
-  // cached references
-  GamePanel gamePanel; // to GamePanel for intersection checking - seems messy, better way?
-  IClickable hoveredObject;
-  IClickable selectedObject;
+    // cached references
+    GamePanel gamePanel; // to GamePanel for intersection checking - seems messy, better way?
+    IClickable hoveredObject;
+    IClickable selectedObject;
 
-  public GameMouseListener(GamePanel gamePanel)
-  {
-    this.gamePanel = gamePanel;
-  }
-
-  @Override
-  public void mousePressed(MouseEvent e)
-  {
-    Point p = e.getPoint();
-    List<IClickable> intersections = gamePanel.getIntersections(p);
-
-    if (intersections.size() == 0)
-    {
-      if (selectedObject != null)
-      {
-        selectedObject.onClickAway(e.getPoint());
-        selectedObject = null;
-      }
-      return;
-    }
-    else if (intersections.size() > 1)
-    {
-      // will have more logic here later, for now return is fine
-      // right now his means stacked objects cannot be clicked where they intersect
-      return; 
-    }
-    if (selectedObject != null)
-    {
-      selectedObject.onClick();
-    }
-    selectedObject = intersections.get(0);
-    selectedObject.onClick();
-  }
-
-  public static boolean isClickable(Object object)
-  {
-    if (object instanceof IClickable)
-    {
-      return true;
-    }
-    return false;
-  }
-
-  /* EVERYTHING BELOW THIS POINT NOT IMPLEMENTED PROPERLY YET - IGNORE
-  @Override
-  public void mouseMoved(MouseEvent e)
-  {
-    Point p = e.getPoint();
-    List<IClickable> intersections = gamePanel.getIntersections(p);
-    System.out.println("1");
-
-    if (intersections.size() == 0)
-    {
-      if (hoveredObject != null)
-      {
-        hoveredObject.onUnhover();
-        hoveredObject = null;
-      }
-      return;
-    }
-    else if (intersections.size() > 1)
-    {
-      return; // will have more logic here later, for now return is fine
+    public GameMouseListener(GamePanel gamePanel) {
+        this.gamePanel = gamePanel;
     }
 
-    intersections.get(0).onHover();
-    if (hoveredObject != null)
-    {
-      hoveredObject = intersections.get(0);
-    }
-  } */
+    @Override
+    public void mousePressed(MouseEvent e) {
+        Point p = e.getPoint();
+        List<IClickable> intersections = gamePanel.getIntersections(p);
 
-  
-
-  /*@Override
-  public void mousePressed(MouseEvent e) {
-    Object source = e.getSource();
-    if (!isClickable(source))
-    {
-      return;
-    }
-    
-    IClickable clickable = (IClickable) source;
-    clickable.onClick();
-  }
-
-  @Override
-  public void mouseEntered(MouseEvent e) {
-    Object source = e.getSource();
-    if (!isClickable(source))
-    {
-      return;
+        if (intersections.size() == 0) {
+            if (selectedObject != null) {
+                selectedObject.onClickAway(e.getPoint());
+                selectedObject = null;
+            }
+            return;
+        } else if (intersections.size() > 1) {
+            return;
+        }
+        if (selectedObject != null) {
+            selectedObject.onClick();
+        }
+        selectedObject = intersections.get(0);
+        selectedObject.onClick();
     }
 
-    IClickable clickable = (IClickable) source;
-    clickable.onHover();
-  }
-
-  @Override
-  public void mouseExited(MouseEvent e) {
-    Object source = e.getSource();
-    if (!isClickable(source))
-    {
-      return;
+    public static boolean isClickable(Object object) {
+        if (object instanceof IClickable) {
+            return true;
+        }
+        return false;
     }
-
-    IClickable clickable = (IClickable) source;
-    clickable.onUnhover();
-  }*/
-
-  
 }
